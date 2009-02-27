662f2274fdeae8688d227cbf9dd14ba2d3728d46
admin@slightphp.com <admin@slightphp.com@421ab7fd-e44e-0410-a24a-e5494ac5ce2e>
2009-02-27 02:15:03 +0000

    add SlightPHPJson plugin

---
+++ trunk/samples/json.php
@@ -0,0 +1,18 @@
+<?php
+require_once("../SlightPHP.php");
+
+$slight=new SlightPHP;
+//$slight->appDir="..";
+$slight->pluginsDir="../plugins";
+//$slight->_debug=true;
+if($slight->loadPlugin("SlightPHPJson")==false){
+	die("loadPlugin error\n");
+}
+$sm = new SlightPHPJson;
+$testObject = new stdclass;
+$testObject->name="SlightPHP";
+$testObject->value=array("min"=>1,"max"=>999);
+print_r($tmp = $sm->encode($testObject));
+echo "\n";
+print_r($sm->decode($tmp));	
+?>
