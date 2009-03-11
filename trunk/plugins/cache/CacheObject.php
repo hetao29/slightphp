3f40dc7cbb8354112dbc2319847496602a7fda07
admin@slightphp.com <admin@slightphp.com@421ab7fd-e44e-0410-a24a-e5494ac5ce2e>
2009-03-11 07:31:08 +0000

    add SCache plugin

--- trunk/plugins/cache/CacheObject.php
+++ trunk/plugins/cache/CacheObject.php
@@ -1,5 +1,6 @@
 <?php
 abstract class CacheObject{
+	abstract function init($params=array());
 	abstract function set($key,$value,$timestamp=-1);
 	abstract function get($key);
 	abstract function del($key);
--- trunk/samples/cache.php
+++ trunk/samples/cache.php
@@ -14,41 +14,51 @@ $slight->appDir=".";
 $slight->defaultZone = "zone";
 $slight->pluginsDir="../plugins";
 $slight->loadPlugin("SCache");
+$slight->loadPlugin("SError");
+
+//cacheengine = array("File","APC","MemCache");
 
 /**
  * File Cache Samples
  */
-//CacheEngine = array("File","APC","MemCache");
-echo phpversion();
-var_dump($result = SCache::getCacheEngine($cacheengine="File"));
-var_dump($result = SCache::getCacheEngine($cacheengine="apc"));
-var_dump($result = SCache::getCacheEngine($cacheengine="memcache"));
-//设置文件存放位置
-/*$cache->setCacheFileDir($dir=);
-//设置目录深度
-$cache->setCacheFileDepth($depth=3);
-//缓存$timestamp时间是秒，-1表永久，默认是一年
-$cache->set($key,$value,$timestamp=);
-$cache->get($key);
-$cache->del($key);
-
-//缓存整个页面
-$cache->pageCache($timestamp);
+/*
+$cache = SCache::getCacheEngine($cacheengine="File");
+if(!$cache){
+	die("File cache engine not exists");
+}
+$cache->init(array("dir"=>"cache","depth"=>3));
+var_dump($cache->set("name",new stdclass));
+var_dump($cache->get("name2"));
+var_dump($cache->get("name"));
+var_dump($cache->del("name"));
 
 */
 /**
  * APC Cache Samples
  */
-//CacheEngine = array("File","APC","MemCached");
-/*
-$result = $cache->setCacheEngine($cacheengine="APC");
-//设置文件存放位置
-$cache->setCacheFileDir($dir=);
-//设置目录深度
-$cache->setCacheFileDepth($depth=3);
-//缓存$timestamp时间是秒，-1表永久，默认是一年
-$cache->set($key,$value,$timestamp=);
-$cache->get($key);
-$cache->del($key);
-*/
+
+$cache = SCache::getCacheEngine($cacheengine="APC");
+if(!$cache){
+	die("File cache engine not exists");
+}
+
+var_dump($cache->set("name",new stdclass));
+var_dump($cache->get("name2"));
+var_dump($cache->get("name"));
+var_dump($cache->del("name"));
+
+/**
+ * Memcache Cache Samples
+ */
+
+$cache = SCache::getCacheEngine($cacheengine="Memcache");
+if(!$cache){
+	die("File cache engine not exists");
+}
+$cache->init(array("host"=>"10.10.221.12","port"=>10001,"permanent"=>true));
+var_dump($cache->set("name",new stdclass));
+var_dump($cache->get("name2"));
+var_dump($cache->get("name"));
+var_dump($cache->del("name"));
+
 ?>
