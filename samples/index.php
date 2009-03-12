3df01d1e8a9e31a31494817395df196cb586f104
admin@slightphp.com <admin@slightphp.com@421ab7fd-e44e-0410-a24a-e5494ac5ce2e>
2009-03-12 07:41:47 +0000

    0.2 samples of index

--- samples/index.php
+++ samples/index.php
@@ -6,18 +6,37 @@
  * http://localhost/samples/index.php/zone-default-entry-a-b-c.html
  *
  */
+/* use static */
+//{{{
 require_once("../SlightPHP.php");
-$slight=new SlightPHP;
-$slight->_debug=true;
-$slight->splitFlag="-_";
-$slight->appDir=".";
-$slight->defaultZone = "zone";
-$slight->pluginsDir="../plugins";
-$slight->loadPlugin("SError");
-$slight->loadPlugin("SSmarty");
-$slight->loadPlugin("SJson");
-if($slight->run()===false){
-	//redirect to 404
+SlightPHP::setDebug(true);
+//var_dump(SlightPHP::getDebug());
+SlightPHP::setAppDir(".");
+//var_dump(SlightPHP::getAppDir());
+SlightPHP::setDefaultZone("zone");
+//var_dump(SlightPHP::getDefaultzone());
+SlightPHP::setDefaultClass("default");
+//var_dump(SlightPHP::getDefaultClass());
+SlightPHP::setDefaultMethod("entry");
+//var_dump(SlightPHP::getDefaultMethod("entry"));
+SlightPHP::setPluginsDir("../plugins");
+//var_dump(SlightPHP::getPluginsDir());
+//var_dump(SlightPHP::loadFile("../plugins/SError.class.php"));
+//var_dump(SlightPHP::loadPlugin("SSmarty"));
+if(SlightPHP::run()===false){
 	die("ERROR ENTRY");
 }
+//}}}
+
+
+/*
+$slight=new SlightPHP;
+$slight->setDefaultZone("zone");
+$slight->setAppDir(".");
+$slight->setPluginsDir("../plugins");
+$slight->loadPlugin("SError");
+if($slight->run()===false){
+	die("ERROR ENTRY");
+}
+*/
 ?>
--- samples/zone/smarty.class.php
+++ samples/zone/smarty.class.php
@@ -1,6 +1,7 @@
 <?php
 class zone_smarty{
 	function pageEntry($inPath){
+		print_r($inPath);
 		$a = new SSmarty;
 		$a->template_dir = "zone/tpl";
 		$a->compile_dir = "zone/tpl";
