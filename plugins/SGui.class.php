58f9a0902f539781d00134904950fb91586d797a
hetao@hetao.name <hetao@hetao.name@421ab7fd-e44e-0410-a24a-e5494ac5ce2e>
2009-08-23 03:55:04 +0000

    1.chang Smarty plugin to SGui.class.php 2.add smarty part function {part path="/main/default/testPart"}

--- plugins/SGui.class.php
+++ plugins/SGui.class.php
@@ -19,37 +19,25 @@
   +----------------------------------------------------------------------+
 */
 
-/**
- * Project:     Smarty: the PHP compiling template engine
- * File:        Smarty.class.php
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * For questions, help, comments, discussion, etc., please join the
- * Smarty mailing list. Send a blank e-mail to
- * smarty-discussion-subscribe@googlegroups.com 
- *
- * @link http://www.smarty.net/
- * @copyright 2001-2005 New Digital Group, Inc.
- * @author Monte Ohrt <monte at ohrt dot com>
- * @author Andrei Zmievski <andrei@php.net>
- * @package Smarty
- * @version 2.6.22
- */
-
 require_once(dirname(__FILE__).DIRECTORY_SEPARATOR."smarty/Smarty.class.php");
-class SSmarty extends Smarty{
+class SGui extends Smarty{
+	public function SGui(){
+		parent::__construct();
+
+		array_push($this->plugins_dir,"plugins_slightphp");
+		$this->compile_dir	= SlightPHP::$appDir.DIRECTORY_SEPARATOR."templates_c";
+		$this->template_dir = SlightPHP::$appDir.DIRECTORY_SEPARATOR."templates";
+
+		$this->left_delimiter = "{{{";
+		$this->right_delimiter = "}}}";
+
+	}
+	public function render($tpl,$parames=array()){
+		foreach($parames as $key=>$value){
+			$this->assign($key,$value);
+		}
+		return $this->fetch($tpl);
+		
+	}
 }
 ?>
---
+++ plugins/smarty/plugins_slightphp/function.part.php
@@ -0,0 +1,48 @@
+<?php
+/*
+  +----------------------------------------------------------------------+
+  | PHP Version 5                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2008 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Authors: Hetal <hetao@hetao.name>                                    |
+  |          SlightPHP <admin@slightphp.com>                             |
+  |          http://www.slightphp.com                                    |
+  +----------------------------------------------------------------------+
+*/
+
+
+/**
+ * Smarty plugin
+ * @package slightphp
+ * @subpackage plugins_slightphp
+ */
+
+
+/**
+ * Smarty {part} function part
+ *
+ * Type:     function<br>
+ * Name:     part<br>
+ * Purpose:  include slightphp part<br>
+ * @author   Hetal <hetao at hetao dot name>
+ * @param array
+ * @param Smarty
+ * @return string
+ */
+function smarty_function_part($params, &$smarty)
+{
+	return !empty($params['path'])?SlightPHP::run($params['path']):"";
+}
+
+/* vim: set expandtab: */
+
+?>
