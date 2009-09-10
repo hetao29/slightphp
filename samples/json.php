67c675db54997762b3d3c66121ac02e04e4a19c2
hetao@hetao.name <hetao@hetao.name@421ab7fd-e44e-0410-a24a-e5494ac5ce2e>
2009-09-10 08:43:23 +0000

    增加samples里 文档说明

--- samples/json.php
+++ samples/json.php
@@ -1,13 +1,38 @@
 <?php
+/*{{{LICENSE
++-----------------------------------------------------------------------+
+| SlightPHP Framework                                                   |
++-----------------------------------------------------------------------+
+| This program is free software; you can redistribute it and/or modify  |
+| it under the terms of the GNU General Public License as published by  |
+| the Free Software Foundation. You should have received a copy of the  |
+| GNU General Public License along with this program.  If not, see      |
+| http://www.gnu.org/licenses/.                                         |
+| Copyright (C) 2008-2009. All Rights Reserved.                         |
++-----------------------------------------------------------------------+
+| Supports: http://www.slightphp.com                                    |
++-----------------------------------------------------------------------+
+}}}*/
+/**
+ * @package SlightPHP
+ * @subpackage samples
+ */
+/**
+ * 这个例子演示如何使用json
+ */
 require_once("global.php");
-SlightPHP::setDebug(true);
-
 
 $sm = new SJson;
 $testObject = new stdclass;
 $testObject->name="SlightPHP";
 $testObject->value=array("min"=>1,"max"=>999);
+/**
+ * 编码
+ */
 print_r($tmp = $sm->encode($testObject));
 echo "\n";
+/**
+ * 解码
+ */
 print_r($sm->decode($tmp));	
 ?>
