6358496152096b04698c6a4d2a218a70868cdf47
panzhiqi <panzhiqi@421ab7fd-e44e-0410-a24a-e5494ac5ce2e>
2009-10-09 16:27:54 +0000

    SUtil中加入得到随机字符串的方法

--- plugins/SUtil.class.php
+++ plugins/SUtil.class.php
@@ -78,6 +78,23 @@ class SUtil{
         $str = mb_strimwidth($str,0,$length*2,$postfix,$encoding);
 		return htmlspecialchars($str, ENT_QUOTES,$encoding);
 	}
-	
+	/**
+	 * get rand string
+	 */
+	static function getRandString($len) {
+		$chars = array(
+			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+			'w', 'x', 'y', 'z',
+			'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+		);
+		$charsLen = count($chars) - 1;
+		shuffle($chars);
+		$output = '';
+		for ($i=0; $i<$len; $i++) {
+			$output .= $chars[mt_rand(0, $charsLen)];
+		}
+		return $output;
+	}
+
 }
 ?>
