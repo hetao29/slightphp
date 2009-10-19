e2295d31ec7f8cfdfab54f8aed7cedf2d144eaf5
panzhiqi <panzhiqi@421ab7fd-e44e-0410-a24a-e5494ac5ce2e>
2009-10-19 03:54:13 +0000

    加入对memche的支持 SCache::set('aa','11')

--- plugins/SCache.class.php
+++ plugins/SCache.class.php
@@ -20,11 +20,19 @@ require_once(dirname(__FILE__).DIRECTORY_SEPARATOR."cache/CacheObject.php");
  */
 class SCache{
 	static $engines=array("file","apc","memcache","memcached");
+	
 	/**
 	 * @param string $engine enum("file","apc","memcache","memcached")
 	 * @return CacheObject
 	 */
-	static function getCacheEngine($engine){
+	static function getCacheEngine($engine='memcache',$server=array()){
+		// global config
+		global $CacheServer;
+		if (empty($server) && isset($CacheServer)) {
+			$server = $CacheServer;
+		}
+		// only support single cache server
+		$cindex = 0;
 		$engine = strtolower($engine);
 		if(!in_array($engine,SCache::$engines)){
 			return false;
@@ -34,11 +42,35 @@ class SCache{
 			return new Cache_APC;
 		}elseif(($engine=="memcache" ||$engine=="memcached")&& extension_loaded("memcache")){
 			require_once(dirname(__FILE__).DIRECTORY_SEPARATOR."cache/Cache_MemCache.php");
-			return new Cache_MemCache;
+			return new Cache_MemCache(array('host'=>$server[$cindex][0],'port'=>$server[$cindex][1]));
 		}elseif($engine=="file"){
 			require_once(dirname(__FILE__).DIRECTORY_SEPARATOR."cache/Cache_File.php");
 			return new Cache_File;
 		}
+	}
+	/**
+	 * check key
+	 */
+	static private function checkKey($key) {
+		return md5($key);
+	}
+	/**
+	 * get
+	 */
+	static public function get($key) {
+		return self::getCacheEngine()->get(self::checkKey($key));
+	}
+	/**
+	 * set
+	 */
+	static public function set($key,$value,$exp=-1) {
+		return self::getCacheEngine()->set(self::checkKey($key),$value,$exp);
+	}
+	/**
+	 * del
+	 */
+	static public function del($key) {
+		return self::getCacheEngine()->del(self::checkKey($key));
 	}
 }
 ?>
