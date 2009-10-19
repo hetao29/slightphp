0fe43e275f58a686067cdfaffb66b068a751f88e
hetao@hetao.name <hetao@hetao.name@421ab7fd-e44e-0410-a24a-e5494ac5ce2e>
2009-10-19 11:48:32 +0000

    SCache 支持和SDb一样的配置方法

--- plugins/SCache.class.php
+++ plugins/SCache.class.php
@@ -21,18 +21,23 @@ require_once(dirname(__FILE__).DIRECTORY_SEPARATOR."cache/CacheObject.php");
 class SCache{
 	static $engines=array("file","apc","memcache","memcached");
 	
+	static $_CacheConfigFile;
+	static $_CachedefaultZone="default";
+	static $_CacheConfigCache;
 	/**
 	 * @param string $engine enum("file","apc","memcache","memcached")
 	 * @return CacheObject
 	 */
-	static function getCacheEngine($engine='memcache',$server=array()){
+	static function getCacheEngine($engine='memcache'){//,$server=array()){
 		// global config
+		/*
 		global $CacheServer;
 		if (empty($server) && isset($CacheServer)) {
 			$server = $CacheServer;
 		}
+		*/
 		// only support single cache server
-		$cindex = 0;
+		//$cindex = 0;
 		$engine = strtolower($engine);
 		if(!in_array($engine,SCache::$engines)){
 			return false;
@@ -42,11 +47,54 @@ class SCache{
 			return new Cache_APC;
 		}elseif(($engine=="memcache" ||$engine=="memcached")&& extension_loaded("memcache")){
 			require_once(dirname(__FILE__).DIRECTORY_SEPARATOR."cache/Cache_MemCache.php");
-			return new Cache_MemCache(array('host'=>$server[$cindex][0],'port'=>$server[$cindex][1]));
+			return new Cache_MemCache;
 		}elseif($engine=="file"){
 			require_once(dirname(__FILE__).DIRECTORY_SEPARATOR."cache/Cache_File.php");
 			return new Cache_File;
 		}
+	}
+	
+	static function setConfigFile($file){
+		SCache::$_CacheConfigFile = $file;
+	}
+	static function getConfigFile(){
+		return SCache::$_CacheConfigFile;
+	}
+	
+	/**
+	 * @param string $zone
+	 * @param string $type	main|query
+	 * @return array
+	 */
+	static function getConfig($zone){
+		if(!SCache::$_CacheConfigFile){return;}
+		
+		$cache = &SCache::$_CacheConfigCache;
+		if(empty($cache[$zone])){
+			$file_data = parse_ini_file(realpath(SCache::$_CacheConfigFile),true);
+			if(isset($file_data[$zone])){
+					$db = $file_data[$zone];
+			}elseif(isset($file_data[SCache::$_CachedefaultZone])){
+					$db = $file_data[SCache::$_CachedefaultZone];
+			}else{
+					return;
+			}
+			foreach($db as $key =>$row){
+				if(strpos($key,"main")!==false){
+					$row = str_replace(":","=",$row);
+					$row = str_replace(",","&",$row);
+					parse_str($row,$out);
+					if(!empty($out)){
+						$cache[$zone][]=$out;
+					}
+				}
+	
+			}
+		}
+		if(isset($cache[$zone])){	
+			$i =  array_rand($cache[$zone]);
+			return $cache[$zone][$i];
+		}else return array();
 	}
 	/**
 	 * check key
