4f84ec6637b704801f8be49e2fd11aea85d59914
hetao@hetao.name <hetao@hetao.name@421ab7fd-e44e-0410-a24a-e5494ac5ce2e>
2010-07-23 13:02:21 +0000

    examples fixed

--- samples/app/index/language.page.php
+++ samples/app/index/language.page.php
@@ -1,7 +1,7 @@
 <?php
 class index_language{
 	function pageEntry($inPath){
-		SLanguage::setLanguageDir(SlightPHP::$appDir."../locale");
+		SLanguage::setLanguageDir(SlightPHP::$appDir."/../locale");
 		SLanguage::setLocale("en");
 		echo SLanguage::tr("name","main");
 		SLanguage::setLocale("zh-CN");
