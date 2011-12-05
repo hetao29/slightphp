aef9848da36dc98b443cfe65c8e9cc2f13ce2a5a
hetao@hetao.name <hetao@hetao.name@421ab7fd-e44e-0410-a24a-e5494ac5ce2e>
2011-12-05 11:35:12 +0000

    增加SRoute路由功能

---
+++ plugins/SRoute.class.php
@@ -0,0 +1,63 @@
+<?php
+/*{{{LICENSE
++-----------------------------------------------------------------------+
+| SlightPHP Framework                                                   |
++-----------------------------------------------------------------------+
+| This program is free software; you can redistribute it and/or modify  |
+| it under the terms of the GNU General Public License as published by  |
+| the Free Software Foundation. You should have received a copy of the  |
+| GNU General Public License along with this program.  If not, see      |
+| http://www.gnu.org/licenses/.                                         |
+| Copyright (C) 2008-2009. All Rights Reserved.                         |
++-----------------------------------------------------------------------+
+| Supports: http://www.slightphp.com                                    |
++-----------------------------------------------------------------------+
+}}}*/
+
+/**
+ * @package SlightPHP
+ */
+class SRoute{
+	private static $_RouteConfigFile;
+	private static $_Routes=array();
+	static function setConfigFile($file){
+		self::$_RouteConfigFile= $file;
+		self::$_Routes = parse_ini_file(self::$_RouteConfigFile,true);
+		self::parse();
+	}
+	static function getConfigFile(){
+		return self::$_RouteConfigFile;
+	}
+	static function set(array $route){
+		self::$_Routes = array($route);
+		self::parse();
+	}
+	private static function parse(){
+		$splitFlag = SlightPHP::getSplitFlag();
+		$splitFlag = $splitFlag{0};
+		foreach(self::$_Routes as $route){
+			$pattern = $route['pattern'];
+			foreach($route as $k=>$v){
+				if(preg_match("/:\w+/",$k)){
+					$pattern = str_replace("$k","($v)",$pattern);
+				}
+			}
+			if(preg_match_all("/$pattern/sm",$_SERVER['PATH_INFO'],$_m)){
+				array_shift($_m);
+				if(!empty($_m)){
+					$params = "";
+					foreach($_m as $_m2){
+						$params.=$splitFlag.$_m2[0];
+					}
+					$zone = empty($route['zone']) ? SlightPHP::getDefaultZone() : $route['zone'];
+					$page = empty($route['page']) ? SlightPHP::getDefaultPage() : $route['page'];
+					$entry = empty($route['entry']) ? SlightPHP::getDefaultEntry() : $route['entry'];
+					$_SERVER['PATH_INFO'] = "{$zone}{$splitFlag}{$page}{$splitFlag}{$entry}{$params}";
+					break;
+				}
+			}
+
+		}
+
+	}
+}
