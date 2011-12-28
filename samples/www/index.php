36eec84e4052eb1a5b0f9c0f279db93dbfa20268
hetao@hetao.name <hetao@hetao.name@421ab7fd-e44e-0410-a24a-e5494ac5ce2e>
2011-12-28 02:13:59 +0000

    samples 调整成更合理的布局

---
+++ samples/www/index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * sample to test
+ *
+ * http://localhost/samples/www/index.php/zone/default/entry/a/b/c
+ * http://localhost/samples/www/index.php/zone-default-entry-a-b-c.html
+ *
+ */
+require_once("global.php");
+
+
+SlightPHP::setDebug(true);
+SlightPHP::setAppDir(ROOT_APP);
+SlightPHP::setDefaultZone("index");
+SlightPHP::setDefaultPage("main");
+SlightPHP::setDefaultEntry("entry");
+SlightPHP::setSplitFlag("-_.");
+
+//{{{
+SDb::setConfigFile(ROOT_CONFIG. "/db.ini");
+SRoute::setConfigFile(ROOT_CONFIG."/route.ini");
+SCache::setConfigFile(ROOT_CONFIG."/cache.ini");
+//}}}
+if(($r=SlightPHP::run())===false){
+	var_dump("404 error");
+}else{
+	var_dump($r);
+}
