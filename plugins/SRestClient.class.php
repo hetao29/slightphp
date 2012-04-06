6be674e6965d382684611c106ca1dfba8bda723f
hetao@hetao.name <hetao@hetao.name@421ab7fd-e44e-0410-a24a-e5494ac5ce2e>
2012-04-06 10:05:42 +0000

    SRestClient更新

--- plugins/SRestClient.class.php
+++ plugins/SRestClient.class.php
@@ -16,5 +16,52 @@
 /**
  * @package SlightPHP
  */
-abstract class SRestClient {
+if(!defined("SLIGHTPHP_PLUGINS_DIR"))define("SLIGHTPHP_PLUGINS_DIR",dirname(__FILE__));
+require_once(SLIGHTPHP_PLUGINS_DIR."/SConfig.class.php");
+require_once(SLIGHTPHP_PLUGINS_DIR."/rest/Rest_Http.class.php");
+class SRestClient extends Rest_Http{
+	private static $_config;
+	static function setConfigFile($file){
+		self::$_config = new SConfig;
+		self::$_config->setConfigFile($file);
+	}
+	static function getConfigFile(){
+		return self::$_config->getConfigFile();
+	}
+	/**
+	 * @param string $zone
+	 * @param string $type	main|query
+	 * @return array
+	 */
+	static function getConfig($zone){
+		return self::$_config->listConfig($zone,".*");
+	}
+	static function useConfig($zone){
+		self::addServers(self::getConfig($zone));
+	}
+	private static $_requests=array();
+	static function addRequest($zone="default",$parameters=array(),$key=null,$method="GET"){
+		self::useConfig($zone);
+		self::addServers(self::getConfig($zone));
+		$para = "";
+		if(!empty($parameters)){
+			$para = http_build_query($parameters);
+		}
+
+		$hash = $zone.$para.$method.$key;
+		$server = self::getServer($hash);
+		$url = "http://".$server['host']."/".$server['path'];
+		$request = array("url"=>$url,"parameters"=>$parameters,"timeout"=>$server['timeout'],"method"=>$method);
+		if(empty($key)){
+			array_push(self::$_requests,$request);
+		}else{
+			self::$_requests[$key]=$request;
+		}
+		return self::$_requests;
+	}
+	static function request(){
+		require_once(SLIGHTPHP_PLUGINS_DIR."/SHttp.class.php");
+		$http = new SHttp;
+		return $http->getArray(self::$_requests);
+	}
 }
