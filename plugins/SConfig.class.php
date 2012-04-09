b1c50e637a8b07cb0296d51a71f2a802b46ec17b
hetao@hetao.name <hetao@hetao.name@421ab7fd-e44e-0410-a24a-e5494ac5ce2e>
2012-04-09 10:42:53 +0000

    SConfig支持新的配置文件格式

--- plugins/SConfig.class.php
+++ plugins/SConfig.class.php
@@ -14,8 +14,9 @@
 +-----------------------------------------------------------------------+
 }}}*/
 if(!defined("SLIGHTPHP_PLUGINS_DIR"))define("SLIGHTPHP_PLUGINS_DIR",dirname(__FILE__));
+define("SCONFIG_FLAG_OBJECT",	1);
+define("SCONFIG_FLAG_ARRAY",	2);
 class SConfig{
-
 		private $_ConfigFile;
 		private $_ConfigCache;
 		private $_DefaultZone="default";
@@ -70,5 +71,69 @@ class SConfig{
 					return $cache[$zone][$key];
 				}
 				return array();
+		}
+		/**
+		  * 支持新的conf配置格式(类似nginx的配置)
+		  * @param int $flag 返回类型Array|Object,默认是Object
+		  * @param boolean $allowMultiValue 允许配置文件存在相同的key(多个相同的key,会自动变成数组)
+		  * @return mixed $result
+		  **/
+		public function parse($flag=SCONFIG_FLAG_OBJECT,$allowMultiValue=true){
+			$content = file(self::getConfigFile());
+			$result=null;
+			$path = array();
+			$_deep=0;
+			foreach($content as $line){
+				//去掉注释,#号表示注释
+				$line = preg_replace("/^(\s*)#(.*)/m","",$line);
+				//找出key,value对应
+				preg_match_all("/(\w+)([\s:]+)([\{\['\"]*)(.*?)\\3([;\{\}\[\]])/S",$line,$_matches,PREG_SET_ORDER);
+				//print_r($_matches);exit;
+				if(!empty($_matches)){
+					foreach($_matches as $_m){
+						if(!empty($_m)){
+							$key = ($_m[1]);
+							$value = ($_m[4]);
+							$object = ($_m[5]);
+							$path[$_deep]=$key;
+							if($object !="{" && $object!="["){
+								self::_setData($result,$path,$value,$flag,$allowMultiValue);
+							}else{
+								$_deep++;
+								self::_setData($result,$path,null,$flag,$allowMultiValue);
+							}
+						}
+					}
+				}else{
+					preg_match("/([\]\}]+).*?/",$line,$_m);
+					if(!empty($_m)){
+						$_deep--;
+						array_pop($path);
+					}
+
+
+				}
+			}
+			return $result;
+		}
+		private function _setData(&$arr,$path,$value,$flag=SCONFIG_FLAG_OBJECT,$allowMultiValue=true){
+			$tmp = &$arr;
+			foreach ($path as $segment) {
+				if($flag==SCONFIG_FLAG_ARRAY){
+					$tmp = &$tmp[$segment];
+				}else{
+					$tmp = &$tmp->$segment;
+				}
+			}
+			if(isset($tmp) && $allowMultiValue==true){
+				if(is_array($tmp)){
+					array_push($tmp,$value);
+				}else{
+					$tmp2=array($tmp,$value);
+					$tmp = $tmp2;
+				}
+			}else{
+				$tmp=$value;
+			}
 		}
 }
