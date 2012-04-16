2e9ea911f59f2974f8be1c059b4fb8eea6005131
hetao@hetao.name <hetao@hetao.name@421ab7fd-e44e-0410-a24a-e5494ac5ce2e>
2012-04-16 12:01:05 +0000

    SConfig 优化

--- plugins/SConfig.class.php
+++ plugins/SConfig.class.php
@@ -85,6 +85,9 @@ class SConfig{
 		//保存临时变量,单引号,双引号里特殊字符
 		$content = preg_replace_callback("/(\S+?)[\s:]+([\'\"])(.*?)\\2;/m",array("SConfig","_tmpData"),$content);
 		$result = new stdclass;
+		//获取最直接的k,v值
+		$_data = self::_getKV($content);
+		$result = (object) array_merge((array) $result, (array) $_data);
 		self::_split($content,$result);
 		self::$_result[$cacheKey]=$result;
 		return $result;
@@ -98,32 +101,34 @@ class SConfig{
 		self::$_tmpData[$key]=$matches[3];
 		return $matches[1].":".$key.";";
 	}
+	private function _getKV($string) {
+		$_data = new stdclass;
+		$tmp_string = preg_replace("/([\w\.\-\_]+?)([\s:]*)\{(([^{}]+|(?R))+)\}/","",$string);
+		preg_match_all("/([\w\.\-\_]+)[\s:]+(.+?);/",$tmp_string,$_matches2,PREG_SET_ORDER);
+		if(!empty($_matches2)){
+			foreach($_matches2 as $_m2){
+				$key = $_m2[1];
+				$value= $_m2[2];
+				if(isset(self::$_tmpData[$value])){$value = self::$_tmpData[$value];}
+				if(isset($_data->$key)){
+					if(is_array($_data->$key)){
+						array_push($_data->$key,$value);
+					}else{
+						$tmp2=array($_data->$key,$value);
+						$_data->$key = $tmp2;
+					}
+				}else{
+					$_data->$key = $value;
+				}
+			}
+		}
+		return $_data;
+	}
 	private function _split ($string,&$result) {
 		preg_match_all("/([\w\.\-\_]+?)[:\s]*\{(([^{}]*|(?R))+)\}/xms",$string,$matches,PREG_SET_ORDER);
 		if (!empty($matches)) {
 			foreach($matches as $m){
-				//找出普通的k,v,需要把{}里的给无视
-				$tmp_string = preg_replace("/([\w\.\-\_]+?)([\s:]*)\{(([^{}]+|(?R))+)\}/","",$m[2]);
-				preg_match_all("/([\w\.\-\_]+)[\s:]+(.+?);/",$tmp_string,$_matches2,PREG_SET_ORDER);
-				$_data=new stdclass;
-				if(!empty($_matches2)){
-					foreach($_matches2 as $_m2){
-						$key = $_m2[1];
-						$value= $_m2[2];
-						//找回被替换的值
-						if(isset(self::$_tmpData[$value])){$value = self::$_tmpData[$value];}
-						if(isset($_data->$key)){
-							if(is_array($_data->$key)){
-								array_push($_data->$key,$value);
-							}else{
-								$tmp2=array($_data->$key,$value);
-								$_data->$key = $tmp2;
-							}
-						}else{
-							$_data->$key = $value;
-						}
-					}
-				}
+				$_data = self::_getKV($m[2]);
 				if(!isset($result->$m[1])){
 					if(!is_array($result)){
 						$result->$m[1] = $_data;
