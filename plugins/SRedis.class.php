7f70f669bb0c865fa19c6fb2c417acd0f079c37f
hetao@hetao.name <hetao@hetao.name@421ab7fd-e44e-0410-a24a-e5494ac5ce2e>
2012-11-15 06:53:20 +0000

    增加SRedis

---
+++ plugins/SRedis.class.php
@@ -0,0 +1,55 @@
+<?php
+/*{{{LICENSE
++-----------------------------------------------------------------------+
+| SlightPHP Framework                                                   |
++-----------------------------------------------------------------------+
+| This program is free software; you can redistribute it and/or modify  |
+| it under the terms of the GNU General Public License as published by  |
+| the Free Software Foundation. You should have received a copy of the  |
+| GNU General Public License along with this program.  If not, see      |
+| http://www.gnu.org/licenses/.                                         |
+| Copyright (C) 2008-2009. All Rights Reserved.                         |
++-----------------------------------------------------------------------+
+| Supports: http://www.slightphp.com                                    |
++-----------------------------------------------------------------------+
+}}}*/
+
+/**
+ * @package SlightPHP
+ */
+if(!defined("SLIGHTPHP_PLUGINS_DIR"))define("SLIGHTPHP_PLUGINS_DIR",dirname(__FILE__));
+require_once(SLIGHTPHP_PLUGINS_DIR."/SConfig.class.php");
+class SRedis{
+	public function __construct(){
+	}
+	private static $_config;
+	private static $_rc;
+	
+	static function setConfigFile($file){
+		self::$_config = $file;
+	}
+	static function getConfig($zone=null){
+		return SConfig::getConfig(self::$_config,$zone);
+	}
+	public function useConfig($zone){
+		$hosts=array();
+		$cfg = self::getConfig($zone);
+		if(empty($cfg))$cfg=self::getConfig("default");
+		if(!empty($cfg)){
+			foreach($cfg as $host){
+			$hosts[]=$host->host.":".$host->port;
+			}
+		}
+		$this->_rc = new RedisArray($hosts);
+	}
+	public function __call($name,$args){
+		return call_user_method_array($name,$this->_rc,$args);
+	}
+}
+$s = new SRedis;
+$s->setConfigFile("../samples/config/redis.ini");
+$s->useConfig("default");
+var_dump($s->set("D","ww"));
+var_dump($s->get("D"));
+var_dump($s->zAdd('k1', 0, 'val0'));
+var_dump($s->zAdd('k1', 1, 'val1'));
