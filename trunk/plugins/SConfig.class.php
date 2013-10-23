2c0a4c700d59775443904d19e445fd7b5b96363f
hetao@hetao.name <hetao@hetao.name@421ab7fd-e44e-0410-a24a-e5494ac5ce2e>
2013-10-23 06:40:11 +0000

    change file_exists to is_file

--- trunk/plugins/SConfig.class.php
+++ trunk/plugins/SConfig.class.php
@@ -122,7 +122,7 @@ class SConfig{
 				if (!empty($_ENV['TMPDIR'])) { return realpath( $_ENV['TMPDIR']); }
 				if (!empty($_ENV['TEMP'])) { return realpath( $_ENV['TEMP']); }
 				$tempfile=tempnam(uniqid(rand(),TRUE),'');
-				if (file_exists($tempfile)) {
+				if (is_file($tempfile)) {
 					unlink($tempfile);
 					return realpath(dirname($tempfile));
 				}
