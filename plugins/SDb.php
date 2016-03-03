7d148e92622bf62177a89cabb135138e2e45fded
hetao <hetao@talkweb.com.cn>
2016-03-03 16:44:31 +0800

    v3.0-beta 支持 composer

--- plugins/SDb.php
+++ plugins/SDb.php
@@ -14,7 +14,7 @@
   +-----------------------------------------------------------------------+
   }}}*/
 if(!defined("SLIGHTPHP_PLUGINS_DIR"))define("SLIGHTPHP_PLUGINS_DIR",dirname(__FILE__));
-require_once(SLIGHTPHP_PLUGINS_DIR."/SConfig.class.php");
+require_once(SLIGHTPHP_PLUGINS_DIR."/SConfig.php");
 require_once(SLIGHTPHP_PLUGINS_DIR."/db/DbData.php");
 require_once(SLIGHTPHP_PLUGINS_DIR."/db/DbObject.php");
 require_once(SLIGHTPHP_PLUGINS_DIR."/db/Db.php");
