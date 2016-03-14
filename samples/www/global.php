9c548581e54a40c28590293d8846ae891034aca4
hetao <hetao@talkweb.com.cn>
2016-03-14 10:29:08 +0800

    Fix 命令行模式下的 E_NOTICE

--- samples/www/global.php
+++ samples/www/global.php
@@ -9,6 +9,7 @@ require_once(ROOT_SLIGHTPHP."/vendor/autoload.php");
 //{{{
 spl_autoload_register(function($class){
 	$file = SlightPHP::$appDir."/".str_replace("_","/",$class).".class.php";
-	if(file_exists($file)) return require_once($file);
+	if(is_file($file)) return require_once($file);
+
 });
 //}}}
